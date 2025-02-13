8d6051f0bfac87402348a4588b6c56e5d84f7718
Vivek Mishra <v.mishra@f5.com>
2025-02-13 19:24:31 +0530

    tap gesture handling

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import React from "react";
 import { Canvas, Circle, Group } from "@shopify/react-native-skia";
-import { SafeAreaView } from "react-native";
+import { useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
 const App = () => {
   const width = 256;
   const height = 256;
-  const r = width * 0.33;
+  const r = useSharedValue(width * 0.33);
+  const color = useSharedValue("cyan");
+
+  const tap = Gesture.Tap()
+    .onBegin(() => {
+      r.value = r.value + 10;
+      color.value = "blue";
+    })
+    .onFinalize(() => {
+      r.value = width * 0.33;
+      color.value = "cyan";
+    });
   return (
-    <SafeAreaView>
-      <Canvas style={{ width, height }}>
-        <Group blendMode="multiply">
-          <Circle cx={r} cy={r} r={r} color="cyan" />
-          <Circle cx={width - r} cy={r} r={r} color="magenta" />
-          <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
-        </Group>
-      </Canvas>
-    </SafeAreaView>
+    <GestureHandlerRootView style={styles.container}>
+      <GestureDetector gesture={tap}>
+        <Canvas style={{ width, height }}>
+          <Circle cx={r} cy={r} r={r} color={color} />
+        </Canvas>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 };
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+  },
+});
